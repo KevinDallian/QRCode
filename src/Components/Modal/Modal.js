c24283053fd8ce70e1fd752192c0077412adac35
Kevin Dallian <kevindallian@gmail.com>
2024-03-25 14:49:50 +0700

    added logic for serialization buttons

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,13 @@
+import './Modal.css'
+
+export default function Modal({children}) {
+    return (
+        <>
+        <div className='modal'>
+            <div className='modal-content'>
+                {children}
+            </div>
+        </div>
+        </>
+    );
+}
--- src/Pages/Serialisasi/Serialisasi.js
+++ src/Pages/Serialisasi/Serialisasi.js
@@ -3,36 +3,88 @@ import { Link } from 'react-router-dom';
 import ActionButton from '../../Components/ActionButton/ActionButton';
 import ReactTable from '../../Components/Table/ReactTable';
 import { useState } from 'react';
+import Modal from '../../Components/Modal/Modal';
 
 export default function Serialisasi(){
-    const headers = ['No', 'Order ID', 'Status'];
-    const [showModal, setShowModal] = useState(false);
+    const headers = ['No', 'Order ID', 'Masterbox ID', 'Manufacture Date', 'Status'];
+    const [showJobModal, setShowJobModal] = useState(false);
+    const [showEndModal, setShowEndModal] = useState(false);
     const [currentJob, setCurrentJob] = useState({});
     const [data, setData] = useState([]);
 
-    const toggleModal = () => {
-        setShowModal(!showModal);
+    const jobs = [
+        {id : "J001", productID : "PR001", batchNo : "23032024/01", expiredDate : new Date("2024-03-29").toString(), quantity : 10, jobStatus : "Active"},
+    ];
+    
+    const loadJob = (index) => {
+        const job = jobs[index];
+        setCurrentJob(job);
+        generateData(job);
+    }
+
+    const endJob = () => {
+        const updatedJob = currentJob;
+        updatedJob.jobStatus = "Ended";
+        setCurrentJob(updatedJob);
+        toggleModal("EndModal");
+    }
+
+    const toggleModal = (modalType) => {
+        if (modalType === "JobModal") {
+            setShowJobModal(!showJobModal);
+        } else {
+            setShowEndModal(!showEndModal);
+        }
+        
+    }
+
+    const formatDate = (date) => {
+        if (!date) return "";
+        if (date instanceof Date) {
+            return date.toISOString().split('T')[0];
+        } else {
+            return new Date(date).toISOString().split('T')[0];
+        }
+    }
+
+    const generateData = (job) => {
+        let generatedData = [];
+        console.log(job.quantity);
+        for (let i=0;i<job.quantity;i++) {
+            const existingDataLength = generatedData.length;
+            const generatedID = `OR${(existingDataLength+1).toString().padStart(3, "0")}`;
+            const newData = {
+                id : generatedID,
+                jobID : job.id,
+                masterboxID : "",
+                manufactureDate : new Date().toDateString,
+                orderStatus : "Not Printed"
+            };
+            generatedData.push(newData);
+        }
+        setData(generatedData)
     }
 
     return (
         <>
-            {showModal && <JobModal showModal={showModal} setShowModal={setShowModal}/>}
+            {showJobModal && <JobModal toggleModal={()=>toggleModal("JobModal")} loadJob={loadJob} jobs={jobs}/>}
             <Link to='/'>Back</Link>
+            {showEndModal && <EndModal toggleModal={()=>toggleModal("EndModal")} endJob={endJob}/>}
             <h1 className='title'>Serialisasi</h1>
             <div className='flex-row job-display'>
                 <div className='col'>
-                    <JobList name={"Job ID"} detail={"J001"}/>
-                    <JobList name={"Produk"} detail={"DKL201030123"}/>
-                    <JobList name={"Batch No"} detail={"202423/01"} />
+                    <JobList name={"Job ID"} detail={currentJob.id}/>
+                    <JobList name={"Produk"} detail={currentJob.productID}/>
+                    <JobList name={"Batch No"} detail={currentJob.batchNo} />
                 </div>
                 <div className='col'>
-                    <JobList name={"Order Quantity"} detail={""}/>
-                    <JobList name={"Expired Date"} detail={""}/>
-                    <JobList name={"Job Status"} detail={"Active"}/>
+                    <JobList name={"Order Quantity"} detail={currentJob.quantity}/>
+                    <JobList name={"Expired Date"} detail={formatDate(currentJob.expiredDate)}/>
+                    <JobList name={"Job Status"} detail={currentJob.jobStatus}/>
                 </div>
                 <div className='flex-column'>
-                    <ActionButton name={"Load Job"} onClickFunction={toggleModal}/>
-                    <ActionButton name={"End Job"} onClickFunction={()=> {console.log("End Job")}}/>
+                    <ActionButton name={"Load Job"} onClickFunction={()=>{toggleModal("JobModal")}}/>
+                    <ActionButton name={"End Job"} onClickFunction={()=> {toggleModal("EndModal")}} disabled={Object.keys(currentJob).length === 0}/>
                 </div>
                 <div className='flex-column'>
                     <ActionButton name={"Print"} onClickFunction={()=> {console.log("Print")}}/>
@@ -47,7 +99,7 @@ export default function Serialisasi(){
                 <p>Cancelled        :</p>
             </div>
             <div id='table'>
-                <ReactTable headers={headers} datas={data}/>
+                <ReactTable headers={headers} datas={data} onClickHandler={(e)=>{console.log(e)}}/>
             </div>
         </>
     );
@@ -55,43 +107,51 @@ export default function Serialisasi(){
 
 function JobList({name, detail}) {
     return (
-        <div className='flex-space-between'>
+        <div className='flex-space-between joblist-box'>
             <h3 className='name'>{name}</h3>
             <h3 className='detail'>{detail}</h3>
         </div>
     );
 }
 
-function JobModal({showModal, setShowModal, currentJob, setCurrentJob}) {
+function JobModal({toggleModal, loadJob, jobs}) {
     const [currentIndex, setCurrentIndex] = useState(null);
     const modalHeaders = ["ID Job", "ID Produk", "Batch No", "Expired Date", "Order Quantity", "Job Status"];
-    const jobs = [
-        {id : "J001", productID : "PR001", batchNo : "23032024/01", expiredDate : new Date("2024-03-29").toString(), quantity : 100, jobStatus : "Active"},
-    ];
-    
-    const toggleModal = () => {
-        setShowModal(!showModal);
+
+    const onClickTableModal = (index) => {
+        setCurrentIndex(index);
     }
 
-    const loadJob = (job) => {
-        setCurrentJob(job);
+    const setJob = (jobIndex) => {
+        loadJob(jobIndex);
         toggleModal();
     }
 
     return (
-        <div className='modal'>
-            <div className='modal-content'>
-                <div className='flex-space-between'>
-                    <h1>Load Job</h1>
-                    <div className='flex-column'>
-                        <button className='modal-btn' onClick={loadJob}>Load</button>
-                        <button className='modal-btn' onClick={toggleModal}>Cancel</button>
-                    </div>
-                </div>
-                <div className=''>
-                    <ReactTable headers={modalHeaders} datas={jobs} onClickHandler={(e)=>{setCurrentIndex(e.target.value)}}/>
+        <Modal>
+            <div className='flex-space-between'>
+                <h1>Load Job</h1>
+                <div className='flex-column'>
+                    <button className='modal-btn' onClick={()=>{setJob(currentIndex)}}>Load</button>
+                    <button className='modal-btn' onClick={toggleModal}>Cancel</button>
                 </div>
             </div>
-        </div>
+            <div className=''>
+                <ReactTable headers={modalHeaders} datas={jobs} currentIndex={currentIndex} onClickHandler={onClickTableModal}/>
+            </div>
+        </Modal>
+    );
+}
+
+function EndModal({toggleModal, endJob}){
+    return (
+        <Modal>
+            <h1>End Job</h1>
+            <p>Apakah ingin melakukan End Job?</p>
+            <div className='flex-row'>
+                <button onClick={toggleModal}>Cancel</button>
+                <button onClick={()=>{endJob()}}>End</button>
+            </div>
+        </Modal>
     );
 }
